4d2513a3438c5acef7618af67062e50dccb37cb2
talhachinchali2 <talha_c@lattech.in>
2025-08-13 13:02:48 +0530

    corrected the code

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,21 +3,21 @@ import {
   IconCamera,
   IconChartBar,
   IconDashboard,
-  IconDatabase,
+  
   IconFileAi,
   IconFileDescription,
-  IconFileWord,
+  
   IconFolder,
   IconHelp,
-  IconInnerShadowTop,
+  
   IconListDetails,
-  IconReport,
+  
   IconSearch,
   IconSettings,
   IconUsers,
 } from "@tabler/icons-react"
 
-import { NavDocuments } from "@/components/nav-documents"
+// import { NavDocuments } from "@/components/nav-documents"
 import { NavMain } from "@/components/nav-main"
 import { NavSecondary } from "@/components/nav-secondary"
 import { NavUser } from "@/components/nav-user"
